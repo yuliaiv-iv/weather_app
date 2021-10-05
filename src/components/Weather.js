c0b279fba557579bbf5d226eea12169eed66924b
Yulia Ivanova <yuliaiv.yulia@yandex.ru>
2021-10-05 11:16:24 -0400

    changed weather component

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,23 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import Loading from './Loading';
-import { url, upperLetter, convertDate, setNumber, imageUrl } from '../utils/config';
-
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import {
+  url,
+  upperLetter,
+  convertDate,
+  setNumber,
+  imageUrl,
+} from "../utils/config";
 
 function Weather({ location, result, error }) {
-
   const [weatherData, setWeatherData] = useState([]);
   const [loading, setLoading] = useState(false);
   const { lat, lng } = location;
   const { daily, current } = weatherData;
   const errorMsg = "Error... Search for other city or location";
 
-
   useEffect(() => {
     function getWeather() {
       setLoading(true);
       return fetch(url(lat, lng))
         .then((res) => {
-          return res.json()
+          return res.json();
         })
         .then((data) => {
           setWeatherData(data);
@@ -34,53 +37,61 @@ function Weather({ location, result, error }) {
 
   return (
     <>
-      {weatherData.length === 0 ? <Loading /> :
+      {weatherData.length === 0 ? (
+        <Loading />
+      ) : (
         <div className="content">
-          <h1 className={`${error ? 'error' : ''}`}>
+          <h1 className={`${error ? "error" : ""}`}>
             {error ? errorMsg : upperLetter(result)}
           </h1>
-          {loading ? <Loading /> :
-            <>
-              <div className="current">
-                <h2>{setNumber(current.temp)} °​C</h2>
-                <img
-                  className="image"
-                  src={imageUrl(current.weather[0].icon)}
-                  alt="weather"
-                />
-                <h3>{upperLetter(current.weather[0].description)}</h3>
-              </div>
-              <table>
-                <tbody>
-                  <tr>
-                    <th className="field">Week day</th>
-                    <th></th>
-                    <th>Low</th>
-                    <th>High</th>
-                    <th>Humidity %</th>
-                    <th>Wind km/hr</th>
-                  </tr>
-                  {daily.map((item, index) => (
-                    <tr key={index}>
-                      <td className="day">{convertDate(item.dt)}</td>
-                      <td>
-                        <img
-                          src={imageUrl(item.weather[0].icon)}
-                          alt="weather"
-                        />
-                      </td>
-                      <td>{setNumber(item.temp.min)} °</td>
-                      <td>{setNumber(item.temp.max)} °</td>
-                      <td>{item.humidity}</td>
-                      <td>{setNumber(item.wind_speed)}</td>
+          {
+            loading ? (
+              <Loading />
+            ) : error ? (
+              ""
+            ) : (
+              <>
+                <div className="current">
+                  <h2>{setNumber(current.temp)} °​C</h2>
+                  <img
+                    className="image"
+                    src={imageUrl(current.weather[0].icon)}
+                    alt="weather"
+                  />
+                  <h3>{upperLetter(current.weather[0].description)}</h3>
+                </div>
+                <table>
+                  <tbody>
+                    <tr>
+                      <th className="field">Week day</th>
+                      <th></th>
+                      <th>Low</th>
+                      <th>High</th>
+                      <th>Humidity %</th>
+                      <th>Wind km/hr</th>
                     </tr>
-                  ))}
-                </tbody>
-              </table>
-            </>
+                    {daily.map((item, index) => (
+                      <tr key={index}>
+                        <td className="day">{convertDate(item.dt)}</td>
+                        <td>
+                          <img
+                            src={imageUrl(item.weather[0].icon)}
+                            alt="weather"
+                          />
+                        </td>
+                        <td>{setNumber(item.temp.min)} °</td>
+                        <td>{setNumber(item.temp.max)} °</td>
+                        <td>{item.humidity}</td>
+                        <td>{setNumber(item.wind_speed)}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </>
+            )
           }
         </div>
-      }
+      )}
     </>
   );
 }
